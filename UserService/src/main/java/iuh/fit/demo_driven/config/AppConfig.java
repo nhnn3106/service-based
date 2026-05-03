3520d6adab747fcae113351fb28eeac1f85b288c
unknown <nnguyennhan365@gmail.com>
2026-05-03 15:47:36 +0700

    oko

--- UserService/src/main/java/iuh/fit/demo_driven/config/AppConfig.java
+++ UserService/src/main/java/iuh/fit/demo_driven/config/AppConfig.java
@@ -15,17 +15,17 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:5173")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(false);
-//            }
-//        };
-//    }
+    // @Bean
+    // public WebMvcConfigurer corsConfigurer() {
+    // return new WebMvcConfigurer() {
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    // registry.addMapping("/**")
+    // .allowedOrigins("http://localhost:5173")
+    // .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    // .allowedHeaders("*")
+    // .allowCredentials(false);
+    // }
+    // };
+    // }
 }
